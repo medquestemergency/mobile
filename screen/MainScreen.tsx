557af022a64b67e5f582e380e99d55c92eb40321
michaeljamieson <michaeljamieson10@gmail.com>
2022-09-11 21:39:20 -0400

    first four buttons i think you should store each value in react redux then upon completion to submit the form
    will map out navigation first

--- screen/MainScreen.tsx
+++ screen/MainScreen.tsx
@@ -1,7 +1,22 @@
-import { SafeAreaView, Text, View} from "react-native";
+import {SafeAreaView, Text, View} from "react-native";
+import {Card, Button} from "@rneui/themed";
+import {StyleSheet} from "react-native";
 
-export default () =>{
-    return <SafeAreaView style={{ flex: 1}}>
-        <Text>Hello world</Text>
+export default () => {
+    return <SafeAreaView style={{flex: 1}}>
+        <Card>
+            <Card.Title>Where is your pain located?</Card.Title>
+            <Button style={styles.button}>Left Chest</Button>
+            <Button style={styles.button}>Right Chest</Button>
+            <Button style={styles.button}>Substernal</Button>
+            <Button style={styles.button}>Epigastric</Button>
+        </Card>
     </SafeAreaView>
 }
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 6,
+        margin: 5,
+    }
+});
