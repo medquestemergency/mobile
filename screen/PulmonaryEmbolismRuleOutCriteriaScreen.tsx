6d183a7d5f2d179c681fffa763f107f03cddcdbb
michaeljamieson <michaeljamieson10@gmail.com>
2023-01-29 11:19:31 -0500

    used vanilla react native and styles from chat gpt maybe refactor into rneui themed if you have time but this works fine able to get this Form data: Object {                                        "bloodyCough": true,                                        "historyOfDVT": true,                                        "hormoneTherapy": true,                                        "legEdema": true,                                        "recentSurgery": true,                                        "vitalSigns": true,                                      } now to actually send the data to my question slice

--- screen/PulmonaryEmbolismRuleOutCriteriaScreen.tsx
+++ screen/PulmonaryEmbolismRuleOutCriteriaScreen.tsx
@@ -1,30 +1,105 @@
-import {SafeAreaView, Text, View} from "react-native";
+import {SafeAreaView, Switch, Text, TouchableOpacity, View} from "react-native";
 import {Card, Button} from "@rneui/themed";
 import {StyleSheet} from "react-native";
 import useNavigateToPainLengthScreen from "../hooks/navigate/useNavigateToPainLengthScreen";
 import useQuestionCallback from "../hooks/useQuestionCallback";
+import {useForm, Controller} from 'react-hook-form';
+import {useState} from "react";
+
+
+type FormData = {
+    legEdema: boolean;
+    historyOfDVT: boolean;
+    vitalSigns: boolean;
+    bloodyCough: boolean;
+    hormoneTherapy: boolean;
+    recentSurgery: boolean;
+};
+
 
 export default () => {
+    const [formData, setFormData] = useState<FormData>({
+        legEdema: false,
+        historyOfDVT: false,
+        vitalSigns: false,
+        bloodyCough: false,
+        hormoneTherapy: false,
+        recentSurgery: false
+    });
+    const { handleSubmit, register } = useForm();
 
-    const onPress = useQuestionCallback(
-        useNavigateToPainLengthScreen(),
-        'painLocated'
-    )
+    const onSubmit = (data: FormData) => {
+        console.log("Form data:", data);
+    };
 
-    return <SafeAreaView style={{flex: 1}}>
-        <Card>
-            <Card.Title>PERCOLATER?</Card.Title>
-            <Button onPress={() => onPress("Left Chest")} style={styles.button}>Left Chest</Button>
-            <Button onPress={() => onPress("Right Chest")} style={styles.button}>Right Chest</Button>
-            <Button onPress={() => onPress("Substernal")} style={styles.button}>Substernal</Button>
-            <Button onPress={() => onPress("Epigastric")} style={styles.button}>Epigastric</Button>
-        </Card>
-    </SafeAreaView>
-}
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Card>
+                <Card.Title>PERC?</Card.Title>
+                <View style={styles.container}>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputLabel}>Do you have any unilateral leg edema?</Text>
+                        <Switch
+                            value={formData.legEdema}
+                            onValueChange={value => setFormData({ ...formData, legEdema: value })}
+                        />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputLabel}>Do you have any personal history of DVT/PE??</Text>
+                        <Switch
+                            value={formData.historyOfDVT}
+                            onValueChange={value => setFormData({ ...formData, historyOfDVT: value })}
+                        />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputLabel}>Are triage vital signs Heart Rate {">"} 100 or SPO2 {"<"}95%?</Text>
+                        <Switch
+                            value={formData.vitalSigns}
+                            onValueChange={value => setFormData({ ...formData, vitalSigns: value })}
+                        />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputLabel}>Have you noticed any bloody cough?</Text>
+                        <Switch
+                            value={formData.bloodyCough}
+                            onValueChange={value => setFormData({ ...formData, bloodyCough: value })}
+                        />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputLabel}>Do you take any hormone therapy including oral contraceptives?</Text>
+                        <Switch
+                            value={formData.hormoneTherapy}
+                            onValueChange={value => setFormData({ ...formData, hormoneTherapy: value })}
+                        />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputLabel}>Have you had any recent (within 3 months) surgery, air travel {">"} 5 hours, or chemotherapy?</Text>
+                        <Switch
+                            value={formData.recentSurgery}
+                            onValueChange={value => setFormData({ ...formData, recentSurgery: value })}
+                        />
+                    </View>
+                        <Button onPress={handleSubmit(() => onSubmit(formData))}>Submit</Button>
+                </View>
+            </Card>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-    button: {
-        borderRadius: 6,
-        margin: 5,
+    container: {
+        alignItems: "center",
+        marginTop: 20
+    },
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 10
+    },
+    inputLabel: {
+        marginRight: 10
+    },
+    submitButton: {
+        marginTop: 20
     }
 });
