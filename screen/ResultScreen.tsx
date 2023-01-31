a1a2783454aaf249e18eadf488f3c21836cefb79
michaeljamieson <michaeljamieson10@gmail.com>
2023-01-31 13:14:51 -0500

    Scroll View saved the day

--- screen/ResultScreen.tsx
+++ screen/ResultScreen.tsx
@@ -1,21 +1,26 @@
-import {SafeAreaView, Text, TextInput, View} from "react-native";
+import {SafeAreaView, ScrollView, Text, TextInput, View} from "react-native";
 import {Card, Button} from "@rneui/themed";
 import {useAppSelector} from "../app/hooks";
 import {RootState} from "../app/store";
 import ResultView from "../components/ResultView";
 import PulmonaryEmbolismRuleOutCriteriaView from "../components/PulmonaryEmbolismRuleOutCriteriaView";
+import React from "react";
 
 export default () => {
     const questionData = useAppSelector((state: RootState) => state.question);
     const pulmonaryEmbolismRuleOutCriteriaData = useAppSelector((state: RootState) => state.pulmonaryEmbolismRuleOutCriteria);
 
     return <SafeAreaView style={{flex: 1}}>
-        <Card>
-            <Card.Title>RESULTS PAGE</Card.Title>
-            {Object.entries(questionData).map(([key, value], index) => <ResultView name={key} formState={value} key={key + index}/>)}
-        </Card>
-        {
-            questionData.painWorsenDeepBreath.answer == "Yes" && <PulmonaryEmbolismRuleOutCriteriaView formData={pulmonaryEmbolismRuleOutCriteriaData.pulmonaryEmbolismRuleOutCriteria.answer} />
-        }
+        <ScrollView>
+            <Card>
+                <Card.Title>RESULTS PAGE</Card.Title>
+                {Object.entries(questionData).map(([key, value], index) => <ResultView name={key} formState={value}
+                                                                                       key={key + index}/>)}
+            </Card>
+            {
+                questionData.painWorsenDeepBreath.answer == "Yes" && <PulmonaryEmbolismRuleOutCriteriaView
+                    formData={pulmonaryEmbolismRuleOutCriteriaData.pulmonaryEmbolismRuleOutCriteria.answer}/>
+            }
+        </ScrollView>
     </SafeAreaView>
 }
